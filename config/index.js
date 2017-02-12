15003aa14d97c63e6362f08b75c89a395587db66
Q. Zhang <qczhang18@gmail.com>
2017-02-12 16:39:33 -0500

    config changes

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ configValues = require('./config');
 module.exports = {
 
   getDbConnectionString: function() {
-       return 'mongodb://' + configValues.uname + ':' + configValues.pwd + '@ds151059.mlab.com:51059/heroku_v9lvs2wp';
+       return 'mongodb://' + configValues.uname + ':' + configValues.pwd + '@ds151059.mlab.com:51059/heroku_dpcv3nfv';
   }
 }
 
